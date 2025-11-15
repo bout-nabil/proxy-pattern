c070237680849fee3373c91ff6a61ecb19aa0eea
Boutachrafine <boutachrafine.nabil@gmail.com>
2025-11-15 17:18:03 +0100

    Work on ProxyCache

--- src/main/java/nbo/Main.java
+++ src/main/java/nbo/Main.java
@@ -1,15 +1,18 @@
 package nbo;
 
 import nbo.controllers.Client;
+import nbo.proxy.ProxyCache;
 import nbo.services.IService;
 import nbo.services.IServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        IService iService = new IServiceImpl();
+        //IService iService = new IServiceImpl();
+        IService proxy = new ProxyCache();
         Client client = new Client();
-
-        client.setService(iService);
+        client.setService(proxy);
+        System.out.println("Result : " + client.perform(20));
+        System.out.println("Result : " + client.perform(20));
         System.out.println("Result : " + client.perform(20));
     }
 }
